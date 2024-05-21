577e04501ff54d4d0a4c31107e3934b11cb85b84
NoortjeMoelans <145358746+NoortjeMoelans@users.noreply.github.com>
2024-05-21 15:26:54 +0200

    WPL 2

---
+++ README.md
@@ -0,0 +1,11 @@
+# wpl1-2324-start
+
+# Naam
+Noortje Moelans
+
+# Studenten-nummer
+12200689
+
+# Klas
+1DVOA
+
---
+++ script/script.js
@@ -0,0 +1,8 @@
+document.getElementById('showpopup').addEventListener("click", function (){
+    document.getElementById('contactpop-up') .style.display = 'block';
+});
+
+document.getElementById('closepopup').addEventListener('click', function (){
+    document.getElementById('closepopup').style.display = 'none';
+});
+

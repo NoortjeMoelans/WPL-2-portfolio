09fd0c64ad318fa6e0ac4eb856dea5354e62f475
NoortjeMoelans <145358746+NoortjeMoelans@users.noreply.github.com>
2024-05-30 17:23:07 +0200

    wpl2

--- script/script.js
+++ script/script.js
@@ -6,3 +6,17 @@ document.getElementById('closepopup').addEventListener('click', function (){
     document.getElementById('closepopup').style.display = 'none';
 });
 
+
+
+function openPopup() {
+    document.getElementById('contactPopup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('contactPopup').style.display = 'none';
+}
+
+function openEmail() {
+    window.location.href = 'mailto:noortjemelans@gmail.com';
+}
+
